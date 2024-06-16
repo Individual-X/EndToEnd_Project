65a83af3d160ca9e45a880efb44a766abbd93b0d
Sajib Hossain <sajib.hossain.limon@gmail.com>
2024-06-16 06:06:56 +0600

    'Setup file Created'

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+   name='Ened To End ML Project',
+   packages=find_packages(),
+   version='0.1.0',
+   description='Heart Disease Prediction',
+   author='Sajib Hossain',
+   author_email='sajibhossain@gmail.com',
+   install_requires = ['pandas', 'numpy', 'seaborn']
+   )
