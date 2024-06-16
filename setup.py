2d33a7a1a3368ae349d407ad2f8e31b80fbffd53
Sajib Hossain <sajib.hossain.limon@gmail.com>
2024-06-16 06:21:15 +0600

    'Setup.py debuggged'

--- setup.py
+++ setup.py
@@ -1,4 +1,28 @@
 from setuptools import find_packages, setup
+from typing import List
+
+Hypen = '-e .'
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function reads a text file containing a list of Python package requirements
+    and returns a list of these requirements.
+
+    Parameters:
+    file_path (str): The path to the text file containing the requirements.
+
+    Returns:
+    List[str]: A list of Python package requirements.
+
+    Raises:
+    FileNotFoundError: If the specified file does not exist.
+    """
+    requirements = []
+    with open(file_path, 'r') as file_obj:
+        requirements = file_obj.readlines()
+        requirements= [req.replace('\n','') for req in requirements]
+        if Hypen in requirements:
+            requirements.remove(Hypen)
+    return requirements
 
 setup(
    name='Ened To End ML Project',
@@ -7,5 +31,5 @@ setup(
    description='Heart Disease Prediction',
    author='Sajib Hossain',
    author_email='sajibhossain@gmail.com',
-   install_requires = ['pandas', 'numpy', 'seaborn']
+   install_requires = get_requirements('requirements.txt')
    )
